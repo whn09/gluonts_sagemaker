38a3f3c427dd52a0035f3e4448044b293409d0dc
EC2 Default User <ec2-user@ip-172-16-26-207.cn-northwest-1.compute.internal>
2021-04-01 03:52:26 +0000

    remove inference from main.py

--- main.py
+++ main.py
@@ -282,41 +282,6 @@ def train(args):
 
 
 # ------------------------------------------------------------ #
-# Hosting methods                                              #
-# ------------------------------------------------------------ #
-
-def model_fn(model_dir):
-    """
-    Load the gluon model. Called once when hosting service starts.
-
-    :param: model_dir The directory where model files are stored.
-    :return: a model (in this case a Gluon network)
-    """
-    predictor = Predictor.deserialize(Path(os.path.join(args.model_dir, args.algo_name)))
-    return predictor
-
-
-def transform_fn(net, data, input_content_type, output_content_type):
-    """
-    Transform a request using the Gluon model. Called once per request.
-
-    :param net: The Gluon model.
-    :param data: The request payload.
-    :param input_content_type: The request content type.
-    :param output_content_type: The (desired) response content type.
-    :return: response payload and content type.
-    """
-    # we can use content types to vary input/output handling, but
-    # here we just assume json for both
-    parsed = json.loads(data)
-    nda = mx.nd.array(parsed)
-    output = net(nda)
-    prediction = mx.nd.argmax(output, axis=1)
-    response_body = json.dumps(prediction.asnumpy().tolist()[0])
-    return response_body, output_content_type
-
-
-# ------------------------------------------------------------ #
 # Training execution                                           #
 # ------------------------------------------------------------ #
 
