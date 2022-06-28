a6fe31bc94f2662870bca5cd3a0576caaa3591b8
EC2 Default User <ec2-user@ip-172-16-55-41.ec2.internal>
2022-06-28 08:06:04 +0000

    fix install_rpy2.sh bug

--- install_rpy2.sh
+++ install_rpy2.sh
@@ -1,14 +1,14 @@
 # update indices
-sudo apt update -qq
+apt update
 # install two helper packages we need
-sudo apt install -y --no-install-recommends software-properties-common dirmngr apt-transport-https
+apt install -y --no-install-recommends dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 # import the signing key (by Michael Rutter) for these repo
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-# sudo add-apt-repository "deb https://opentuna.cn/CRAN/bin/linux/ubuntu/ $(lsb_release -cs)-cran35/"
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran35/"
-sudo apt update -qq
-sudo apt install -y --no-install-recommends r-base r-base-dev
-sudo apt install -y libcurl4-openssl-dev
-sudo R -e 'install.packages(c("forecast", "nnfor"), repos="https://cloud.r-project.org", dependencies=TRUE)'
+# add-apt-repository "deb https://opentuna.cn/CRAN/bin/linux/ubuntu/ $(lsb_release -cs)-cran40/"
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt update
+apt install -y --no-install-recommends r-base r-base-dev
+apt install -y libcurl4-openssl-dev
+R -e 'install.packages(c("forecast", "nnfor"), repos="https://cloud.r-project.org", dependencies=TRUE)'
 pip install 'rpy2>=2.9.*,<3.*'
